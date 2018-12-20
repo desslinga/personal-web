2271b6e56408eadb2c8497158e935644914f2708
Fides Linga <fides.linga@mail.utoronto.ca>
2018-12-20 16:30:09 -0500

    adding event listener for width

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
+
+
 export class NavComponent implements OnInit {
   navbarOpen: boolean;
+  width: number;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.width = window.innerWidth;
+    if (this.width > 762) {
+      this.navbarOpen = false;
+    }
+  }
+
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
 
   constructor() {
     this.navbarOpen = false;
+    console.log(window);
+    this.width = window.innerWidth;
   }
 
   ngOnInit() {
