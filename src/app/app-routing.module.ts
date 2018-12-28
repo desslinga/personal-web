8692e6ba17d8c9103df4a654625f2c45807990ae
desslinga <fides.linga@mail.utoronto.ca>
2018-12-28 12:37:57 -0500

    directed routing to closetr and work pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { WorkComponent } from './work/work.component';
+import { ClosetrComponent } from './work/closetr/closetr.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/about', pathMatch: 'full'},
   {path: 'home', redirectTo: '/about', pathMatch: 'full'},
   {path: 'about', component: HomeComponent},
   {path: 'gallery', component: GalleryComponent},
+  {path: 'work', component: WorkComponent},
+  {path: 'work/closetr', component: ClosetrComponent}
 ];
 
 @NgModule({
