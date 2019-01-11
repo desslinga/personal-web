19311ba3cf868923e7b1f615b3714c7bb1190c8f
Fides Linga <fides.linga@mail.utoronto.ca>
2019-01-11 15:55:37 -0500

    changed tools used in gallery preview

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -38,18 +38,21 @@ export class GalleryComponent implements OnInit {
        This design was not eventually used for the event, but
        it was still fun making it! This features the CN Tower.
        Designed on Adobe Illustrator, with some finishing touches
-       on ProCreate (iPad Pro).`],
+       on ProCreate (iPad Pro).`,
+      'Adobe Illustrator, ProCreate (with iPad Pro)'],
       ["image-02.png",
        "transitassets-01.png",
        "Toronto Transit",
        `Buses and Trains for TTC, MiWay, and GO Transit. Created
         as part of a brochure design during an internship with
-        GO Transit. Designed on Adobe Illustrator.`],
+        GO Transit. Designed on Adobe Illustrator.`,
+       'Adobe Illustrator'],
       ["image-03.png",
        "plants_v2.png",
        "Potted Plants - Leafy Greens",
        `Part of an ongoing study of leafy plants and pots.
-        Illustration made with ProCreate on iPad Pro.`],
+        Illustration made with ProCreate on iPad Pro.`,
+       'ProCreate (with iPad Pro)'],
       ["image-04.png",
        "uoft_hacks_skyline.png",
        "U of T Hacks - Skyline",
@@ -57,40 +60,47 @@ export class GalleryComponent implements OnInit {
        This design was not eventually used for the event, but
        it was still fun making it! This features the Toronto
        Skyline. Designed on Adobe Illustrator, with some finishing
-       touches on ProCreate (iPad Pro).`],
+       touches on ProCreate (iPad Pro).`,
+      'Adobe Illustrator, ProCreate (with iPad Pro)'],
       ["image-05.png",
        "plants_v1.png",
        "Potted Plants - Succulents",
        `Part of an ongoing study of leafy plants and pots.
-        Illustration made with ProCreate on iPad Pro.`],
+        Illustration made with ProCreate on iPad Pro.`,
+       'ProCreate (with iPad Pro)'],
       ["image-06.png",
        "go_no_brainer_savings.png",
        "GO Transit Back to School",
        `Poster design for 'GO Back to School' promotional event
        for GO Transit. Created during an internship with GO
-       Transit. Designed on Adobe Illustrator.`],
+       Transit. Designed on Adobe Illustrator.`,
+      'Adobe Illustrator'],
       ["image-07.png",
        "go_introduction_3.png",
        "GO Introduction Brochure",
        `Inner pages of brochure designed for 'GO Introduction'
        brochure, as a concept. Created during an internship with
-       GO Transit. Designed on Adobe Illustrator.`],
+       GO Transit. Designed on Adobe Illustrator.`,
+      'Adobe Illustrator'],
       ["image-08.png",
        "go_introduction_2.png",
        "GO Introduction Brochure",
        `Inner pages (History) of brochure designed for 'GO Introduction'
        brochure, as a concept. Created during an internship with
-       GO Transit. Designed on Adobe Illustrator.`],
+       GO Transit. Designed on Adobe Illustrator.`,
+      'Adobe Illustrator'],
       ["image-09.png",
        "girl_with_cats.PNG",
        "Playing with Cats",
        `Illustration made with ProCreate on iPad Pro. I really like
-       cats so I made this one!`],
+       cats so I made this one!`,
+      'Procreate (with iPad Pro)'],
       ["image-10.png",
        "amacss_gaming_night_2.PNG",
        "AMACSS Gaming Night 2 Poster",
        `Poster created for AMACSS' Gaming Night. Illustrated on iPad Pro,
-       and designed on Adobe Illustrator.`],
+       and designed on Adobe Illustrator.`,
+      'Adobe Illustrator, ProCreate (with iPad Pro)'],
       ["image-11.png",
        "uoft_hacks_title.png",
        "U of T Hacks - Title",
@@ -98,27 +108,32 @@ export class GalleryComponent implements OnInit {
        This design was not eventually used for the event, but
        it was still fun making it! This features the title.
        Designed on Adobe Illustrator, with some finishing
-       touches on ProCreate (iPad Pro).`],
+       touches on ProCreate (iPad Pro).`,
+      'Adobe Illustrator, ProCreate (with iPad Pro)'],
       ["image-12.png",
        "go_introduction_1.png",
        "GO Introduction Brochure",
        `Cover of brochure designed for 'GO Introduction'
        brochure, as a concept. Created during an internship with
-       GO Transit. Designed on Adobe Illustrator.`],
+       GO Transit. Designed on Adobe Illustrator.`,
+      'Adobe Illustrator'],
       ["image-13.png",
        "amacss_quiz_night.png",
        "AMACSS Quiz Night",
        `Poster created for AMACSS' Quiz Night. Illustrated on iPad Pro,
-       and designed on Adobe Illustrator.`],
+       and designed on Adobe Illustrator.`,
+      'Adobe Illustrator, ProCreate (with iPad Pro)'],
       ["image-14.png",
        "amacss_gaming_night_1.png",
        "AMACSS Gaming Night 1 Poster",
        `Poster created for AMACSS' Gaming Night. Illustrated on iPad Pro,
-       and designed on Adobe Illustrator.`],
+       and designed on Adobe Illustrator.`,
+      'Adobe Illustrator, ProCreate (with iPad Pro)'],
        ["image-15.png",
         "weekly1_flowergirl.PNG",
         "Flower Girl",
-        `Lorem Ipsum.`]
+        `Portrait of girl with decorative flowers. Part 1 of some quick studies
+         I am trying to do every week or so.`]
     ];
 
     this.screenChangeService = screenchangeservice;
@@ -133,7 +148,8 @@ export class GalleryComponent implements OnInit {
        this.imageListFull[i] = {
          'url': "./assets/images/gallery/archives/" + this.imageList[i][1],
          'title': this.imageList[i][2],
-         'description': this.imageList[i][3]
+         'description': this.imageList[i][3],
+         'tools': this.imageList[i][4]
        };
     }
 
