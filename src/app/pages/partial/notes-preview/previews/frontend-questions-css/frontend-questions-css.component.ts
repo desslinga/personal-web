bc13fc18b61365136ae4387f6e7c26f03434d924
desslinga <fides.linga@mail.utoronto.ca>
2019-04-28 20:33:59 -0400

    succesfully integrated prism js for code snippets.

--- src/app/pages/partial/notes-preview/previews/frontend-questions-css/frontend-questions-css.component.ts
+++ src/app/pages/partial/notes-preview/previews/frontend-questions-css/frontend-questions-css.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HighlightService } from '../../../../../services/highlight.service';
 
 @Component({
   selector: 'app-frontend-questions-css',
   templateUrl: './frontend-questions-css.component.html'
 })
 export class FrontendQuestionsCssComponent implements OnInit {
+  myCode;
 
-  constructor() { }
+  constructor(private highlightService: HighlightService) { }
 
   ngOnInit() {
+    this.highlightService.highlightAll();
   }
 
 }
