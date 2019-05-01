ee4c238c1cea23815dc2559d0cc8bcd3c3596ecc
Fides Linga <fides.linga@mail.utoronto.ca>
2019-05-01 10:22:44 -0400

    Adding question to js part one

--- src/app/services/highlight.service.ts
+++ src/app/services/highlight.service.ts
@@ -7,7 +7,7 @@ import 'clipboard';
 
 import 'prismjs';
 import 'prismjs/plugins/toolbar/prism-toolbar';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+//import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-java';
